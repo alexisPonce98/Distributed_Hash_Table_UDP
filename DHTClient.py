5c25e285a739eda6ff73e30928f310677cfa99e9
Alexis Ponce <ponce.alexis98@gmail.com>
2021-09-25 17:28:23 -0700

    able to send messages form server to client

--- DHTClient.py
+++ DHTClient.py
@@ -1,17 +1,39 @@
 import socket
 import sys
 
+processName:str
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverIP:int
+serverSocket:int
 
-def register():
+#register long_name IP port(s)
+def register(parsedList):
     print("Registering")
+    processName = parsedList[1]
+    print("This is the second input: " + parsedList[1])
+    clientSocket.sendto(parsedList.encode(), (serverIP, serverSocket))
 
-stay = True
-while stay:
-    command = input("Client: ")
-    print(str(command))
-    if command == "register":
+
+wait = False
+while True:
+    while wait:
+        message, serverADDR = clientSocket.recvfrom(2048)
+        print(message)
+        wait = False
+    server = input("What is the server IP and port Number: ")
+    parsedServerCommand = server.split( )
+
+    serverIP = parsedServerCommand[0]
+    serverSocket = int(parsedServerCommand[1])
+
+    command = input("Enter client commands: ")
+    parsed = command.split( )
+    print(parsed)
+
+    if parsed[0] == "register":
         print("Boutta go to register")
-        register()
+        register(command)
+        wait = True
     elif command == "exit":
         break
     #print("Doing something")
--- DHTServer.py
+++ DHTServer.py
@@ -1,1 +1,23 @@
 import socket
+#need to figure out this part
+
+clientName:str
+state:int# 0 = free, 2 = inDHT, 3 = Leader
+userDict = {}
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = input("What is the socket: ")
+serverSocket.bind(('', int(sock)))
+
+def register(parsedList):
+    print("registering")
+    clientName = parsedList[1]
+    print("Got client: " + clientName)
+    userDict[clientName] = [parsedList[2], parsedList[3], 0]
+    print(userDict)
+
+while True:
+    message, clientADDR = serverSocket.recvfrom(2048)
+    decodedMessage = message.decode().upper()
+    print(decodedMessage)
+    serverSocket.sendto("Recieved registration".encode(), clientADDR)
